41d832f75a8a6bac5348a8a0649904f79f06fd9d
Anant Narayan <anantnrg@gmail.com>
2024-01-07 18:49:25 +0530

    lots of stuf

---
+++ template/src/scenes/template_scene.tsx
@@ -0,0 +1,21 @@
+import { makeScene2D, Circle, Txt, Img, Rect, Line, Icon, Latex } from '@motion-canvas/2d';
+import { all, tween, createRef, map, easeInSine, chain, easeInOutSine, waitFor, slideTransition, Direction, easeOutSine, easeInBounce, createSignal, Vector2, waitUntil, easeOutBack } from '@motion-canvas/core';
+import { CodeBlock, insert, lines, range, remove } from '@motion-canvas/2d/lib/components/CodeBlock';
+import ferrisImg from '../assets/ferris.svg';
+import { Copyright } from 'helpers/copyright';
+
+export default makeScene2D(function* (view) {
+    yield view.add(
+        <Copyright text=' Technologs ' />
+    );
+
+    yield view.add(
+        <Img
+            src={ferrisImg}
+            width={900}
+            scale={0.15}
+            x={850}
+            y={470}
+        ></Img>
+    );
+});
