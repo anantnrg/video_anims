18d06a734b484b2d39300a224e032227bfab8825
Anant Narayan <anantnrg@gmail.com>
2024-01-15 11:36:51 +0530

    lots of stuff

--- videos/rust-part-5/src/scenes/00_intro.tsx
+++ videos/rust-part-5/src/scenes/00_intro.tsx
@@ -3,6 +3,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Circle, Rect, Txt, makeScene2D } from "@motion-canvas/2d";
 import {
+  Vector2,
   all,
   createRef,
   easeInOutQuad,
@@ -40,7 +41,7 @@ export default makeScene2D(function* (view) {
   //       <Circle
   //         width={2}
   //         height={2}
-  //         fill="rgba(255, 255, 255, 0.75)"
+  //         fill="rgba(255, 255, 255, 0.35)"
   //         ref={makeRef(circles, i)}
   //         opacity={0}
   //       />
@@ -119,7 +120,12 @@ export default makeScene2D(function* (view) {
   const mainStackFrameCont = createRef<Rect>();
   const mainStackFrame = createRef<Rect>();
   const mainStackFrameTitle = createRef<Txt>();
-  const mainStackFrameSampleValue = createRef<Txt>();
+  const mainStackFrameTitleSep = createRef<Rect>();
+  const highlightBox = createRef<Rect>();
+  const mainStackFrameSampleValueInt = createRef<Txt>();
+  const mainStackFrameSampleValueFloat = createRef<Txt>();
+  const mainStackFrameSampleValueBool = createRef<Txt>();
+  const LIFO = createRef<Txt>();
 
   yield view.add(
     <Rect layout direction="column" alignItems="center" ref={memBox} scale={0}>
@@ -236,10 +242,10 @@ export default makeScene2D(function* (view) {
       direction="column"
       alignItems="center"
       width="100%"
-      height={200}
+      minHeight={200}
       margin={10}
       stroke="cba6f7"
-      lineWidth={4}
+      lineWidth={0}
       radius={15}
       clip
       ref={mainStackFrameCont}
@@ -251,17 +257,25 @@ export default makeScene2D(function* (view) {
         fill="cba6f7"
         marginTop={10}
         text="main"
+        ref={mainStackFrameTitle}
         scale={0}
       />
-      <Rect width="100%" stroke="cba6f7" lineWidth={4} marginTop={10} />
       <Rect
-        layout
+        width="100%"
+        stroke="cba6f7"
+        lineWidth={4}
+        marginTop={10}
+        ref={mainStackFrameTitleSep}
+        opacity={0}
+      />
+      <Rect
         direction="column"
         width="100%"
         height="100%"
-        alignItems="center"
-        justifyContent="center"
         ref={mainStackFrame}
+        gap={30}
+        paddingLeft={20}
+        paddingTop={20}
       />
     </Rect>,
   );
@@ -276,13 +290,87 @@ export default makeScene2D(function* (view) {
       fontFamily="JetBrains Mono"
       fontSize={32}
       fontWeight={600}
-      fill="cdd6f4"
-      ref={mainStackFrameSampleValue}
-      text=""
+      fill="89dceb"
+      ref={mainStackFrameSampleValueInt}
+      text="some_int: 420"
+      opacity={0}
     />,
   );
 
-  yield* waitUntil("program will allocate");
-  yield* mainStackFrameSampleValue().text("value: 420", 0.75, easeInOutQuad);
+  yield* waitUntil("integers");
+  yield* mainStackFrameSampleValueInt().opacity(1, 0.75, easeInOutQuad);
+
+  mainStackFrame().add(
+    <Txt
+      fontFamily="JetBrains Mono"
+      fontSize={32}
+      fontWeight={600}
+      fill="74c7ec"
+      ref={mainStackFrameSampleValueFloat}
+      text="a_float: 3.004"
+      opacity={0}
+    />,
+  );
+
+  yield* waitUntil("floats");
+  yield* mainStackFrameSampleValueFloat().opacity(1, 0.75, easeInOutQuad);
+  mainStackFrame().add(
+    <Txt
+      fontFamily="JetBrains Mono"
+      fontSize={32}
+      fontWeight={600}
+      fill="94e2d5"
+      ref={mainStackFrameSampleValueBool}
+      text="bool: true"
+      opacity={0}
+    />,
+  );
+
+  yield view.add(
+    <Rect
+      width={250}
+      height={60}
+      lineWidth={5}
+      stroke="f38ba8"
+      radius={10}
+      x={-80}
+      y={-20}
+      opacity={0}
+      ref={highlightBox}
+    />,
+  );
+
+  yield view.add(
+    <Txt
+      text=""
+      fontFamily="JetBrains Mono"
+      fontSize={56}
+      fontWeight={800}
+      fill="f9e2af"
+      x={600}
+      ref={LIFO}
+    />,
+  );
+
+  yield* waitUntil("booleans");
+  yield* mainStackFrameSampleValueBool().opacity(1, 0.75, easeInOutQuad);
+
+  yield* waitUntil("are organized");
+  yield* LIFO().text("Last In First Out", 0.75, easeInOutQuad);
+
+  yield* waitUntil("the last value");
+  yield* highlightBox().opacity(1, 0.75, easeInOutQuad);
+  yield* waitUntil("popped off");
+  yield* highlightBox().opacity(0, 0.75, easeInOutQuad);
+  yield* mainStackFrameSampleValueBool().opacity(0, 0.75, easeInOutQuad);
+
+  yield* waitUntil("values in the stack");
+  yield* LIFO().text("", 0.75, easeInOutQuad);
+  yield* all(
+    mainStackFrameTitle().scale(1, 0.75, easeInOutQuad),
+    mainStackFrameTitleSep().opacity(1, 0.75, easeInOutQuad),
+    // mainStackFrameTitleSep().margin((), 0.75, easeInOutQuad),
+    mainStackFrameCont().lineWidth(4, 0.15, easeInOutQuad),
+  );
   /* ---- Memory Allocation End ---- */
 });
